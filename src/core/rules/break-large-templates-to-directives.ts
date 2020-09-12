2d3bcaf7d448165fd434403e0230f2ce415c5638
lgtkaushalya <lgtkaushalya@gmail.com>
2020-09-12 10:20:15 +0530

    Added break-large-templates-to-directives rule

---
+++ src/core/rules/break-large-templates-to-directives.ts
@@ -0,0 +1,19 @@
+import { Rule } from '../types'
+
+export default {
+  id: 'break-large-templates-to-directives',
+  description: 'Large templates tend to have mixed responsibilities',
+  init(parser, reporter) {
+    parser.addListener('end', (event) => {
+      if (event.line > 100) {
+        reporter.error(
+          'Break large templates into directives',
+          event.line,
+          event.col,
+          this,
+          event.raw
+        )
+      }
+    })
+  },
+} as Rule
