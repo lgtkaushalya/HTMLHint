967e0c8e192bf4aa266511f96af4d27aaf14f604
lgtkaushalya <lgtkaushalya@gmail.com>
2020-10-18 23:56:49 +0530

    Added directive replacement suggestion

--- src/core/rules/replace-directive-with-react-component.ts
+++ src/core/rules/replace-directive-with-react-component.ts
@@ -1,18 +1,31 @@
 import { Rule } from '../types'
+var fs = require('fs')
 
 export default {
   id: 'replace-directive-with-react-component',
-  description: 'Large templates tend to have mixed responsibilities',
+  description: 'Replace directive with react component',
   init(parser, reporter) {
-    parser.addListener('end', (event) => {
-      if (event.line > 100) {
-        reporter.error(
-          'Break large templates into directives',
-          event.line,
-          event.col,
-          this,
-          event.raw
-        )
+    parser.addListener('tagstart', (event) => {
+      const globalAny:any = global;
+      const tagName = event.tagName;
+      const matchingComponentName = tagName.charAt(0).toUpperCase() + tagName.slice(1) + 'Component';
+      let dataFilePath = globalAny.rootPath !== undefined ? globalAny.rootPath : process.cwd();
+      dataFilePath = dataFilePath.replace(/\/$/, '') + '/.ng-react-copilot-data/extracted_data.json';
+
+      if (fs.existsSync(dataFilePath)) {
+        const fileData = fs.readFileSync(dataFilePath, 'utf8');
+        const dataObject = JSON.parse(fileData);
+
+        if (dataObject.reactComponents !== undefined
+          && dataObject.reactComponents.includes(matchingComponentName)) {
+          reporter.error(
+            'Replace angular '+ tagName +' directive with react' + tagName + '-component',
+            event.line,
+            event.col,
+            this,
+            event.raw
+          )
+        }
       }
     })
   },
