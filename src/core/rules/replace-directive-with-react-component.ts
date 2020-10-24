c09c62753f6321c229553ae76736c7fc120a1ae8
lgtkaushalya <lgtkaushalya@gmail.com>
2020-10-24 10:50:53 +0530

    Fixed the cammel case not converting issue in replace-directive-with-react-component

--- src/core/rules/replace-directive-with-react-component.ts
+++ src/core/rules/replace-directive-with-react-component.ts
@@ -1,5 +1,6 @@
 import { Rule } from '../types'
 var fs = require('fs')
+const _ = require('lodash')
 
 export default {
   id: 'replace-directive-with-react-component',
@@ -8,14 +9,13 @@ export default {
     parser.addListener('tagstart', (event) => {
       const globalAny:any = global;
       const tagName = event.tagName;
-      const matchingComponentName = tagName.charAt(0).toUpperCase() + tagName.slice(1) + 'Component';
+      const matchingComponentName =  _.upperFirst(_.camelCase(tagName.charAt(0).toUpperCase() + tagName.slice(1) + 'Component'));
       let dataFilePath = globalAny.rootPath !== undefined ? globalAny.rootPath : process.cwd();
       dataFilePath = dataFilePath.replace(/\/$/, '') + '/.ng-react-copilot-data/extracted_data.json';
 
       if (fs.existsSync(dataFilePath)) {
         const fileData = fs.readFileSync(dataFilePath, 'utf8');
         const dataObject = JSON.parse(fileData);
-
         if (dataObject.reactComponents !== undefined
           && dataObject.reactComponents.includes(matchingComponentName)) {
           reporter.error(
